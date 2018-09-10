f7dc1c9b71067a2c828c47ed8d5b4d61c45c1a19
Wouter van Atteveldt <wouter@vanatteveldt.com>
2018-09-10 12:50:57 +0200

    Speed up fixdb (but why)

--- fixdb.py
+++ fixdb.py
@@ -1,5 +1,6 @@
 import django;
 from django.db.models import Count
+from django.db import transaction
 
 django.setup()
 
@@ -7,15 +8,18 @@ from actcode.models import Document, Annotation
 
 tofix = list(Document.objects.filter(title=""))
 print("Fixing",len(tofix), " titles")
-for doc in tofix:
-    title = doc.text.strip().split("\n")[0]
-    doc.title = title
-    doc.save()
+with transaction.atomic():
+    for i, doc in enumerate(tofix):
+        title = doc.text.strip().split("\n")[0]
+        doc.title = title
+        doc.save()
+        if not i % 1000: print(i)
 
 dupes = list(Annotation.objects.values('document_id', 'label_id').annotate(n = Count('id')).filter(n__gte=2))
 print("Fixing", len(dupes), "duplicate annotations")
-for dupe in dupes:
-    ids = list(Annotation.objects.filter(document_id = dupe['document_id'], label_id=dupe['label_id']).values_list('pk', flat=True).order_by('id'))
-    remove = ids[:-1]
-    Annotation.objects.filter(pk__in=remove).delete()
-
+with transaction.atomic():
+    for i, dupe in enumerate(dupes):
+        ids = list(Annotation.objects.filter(document_id = dupe['document_id'], label_id=dupe['label_id']).values_list('pk', flat=True).order_by('id'))
+        remove = ids[:-1]
+        Annotation.objects.filter(pk__in=remove).delete()
+        if not i % 1000: print(i)
