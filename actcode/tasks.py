afdbad0cb17446988e16a8ca4dab96566df16bc2
Wouter van Atteveldt <wouter@vanatteveldt.com>
2018-09-15 13:09:16 +0200

    prevent circular import

--- actcode/tasks.py
+++ actcode/tasks.py
@@ -1,7 +1,7 @@
 from huey.contrib.djhuey import db_task
 import logging
 
-from actcode.ml import ActiveLearn
+import actcode.ml
 from actcode.models import Session
 
 
@@ -9,7 +9,7 @@ from actcode.models import Session
 def populate_todo(session_id, n):
     s = Session.objects.get(pk=session_id)
     print("Populating TODO for session", s)
-    ActiveLearn(s).do_populate_todo(n)
+    actcode.ml.ActiveLearn(s).do_populate_todo(n)
     s.populate_task_id = None   # more efficient to do 1 save, but meh
     s.save()
     print("DONE")
