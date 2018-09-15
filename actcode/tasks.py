d481f6f844500b4c075deb0ed051bef4ab8eb5c5
Wouter van Atteveldt <wouter@vanatteveldt.com>
2018-09-15 13:03:30 +0200

    Add tasks and migration for background tasks

---
+++ actcode/tasks.py
@@ -0,0 +1,15 @@
+from huey.contrib.djhuey import db_task
+import logging
+
+from actcode.ml import ActiveLearn
+from actcode.models import Session
+
+
+@db_task()
+def populate_todo(session_id, n):
+    s = Session.objects.get(pk=session_id)
+    print("Populating TODO for session", s)
+    ActiveLearn(s).do_populate_todo(n)
+    s.populate_task_id = None   # more efficient to do 1 save, but meh
+    s.save()
+    print("DONE")
