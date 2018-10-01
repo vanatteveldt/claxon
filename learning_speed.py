c8a5cfba013026853ead228560c0a2df611cc49d
Wouter van Atteveldt <wouter@vanatteveldt.com>
2018-10-01 20:28:25 +0200

    Re-run and partially shuffle in learning_speed

--- learning_speed.py
+++ learning_speed.py
@@ -1,18 +1,17 @@
+# Silly django stuff
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'claxon.settings'
+import django
+django.setup()
+
 import csv
+import sys
 import logging
 import random
 from functools import partial
-
-import django
-django.setup()
-import sys
-
-
-from actcode.eval import combine
-from actcode.ml import train, evaluate
-
 from math import ceil
 
+from actcode.ml import train, evaluate
 from actcode.models import Project, Annotation, Label
 
 project = Project.objects.get(pk=1)
@@ -22,9 +21,6 @@ all_annotations = list(Annotation.objects.filter(session__project=project, docum
 #project = Project.objects.get(pk=3)
 #all_annotations = list(Annotation.objects.filter(session__project=project, document__gold=False).order_by("id"))
 LAST_EVAL = None
-#x, y = all_annotations[:682], all_annotations[682:]
-#random.shuffle(x)
-#all_annotations = x + y
 N = len(all_annotations)
 I = 10
 out = csv.writer(sys.stdout)
@@ -32,20 +28,30 @@ out.writerow(["n", "i", "label", "tp", "fp", "fn", "tn"])
 
 eval_annotations = list(Annotation.objects.filter(document__gold=True, session__project=project, label=label))
 
-def log_performance(i, model, n):
+def shuffle_annotations(annotations):
+    # Would like to use sessions, but at least crime just doesn't have any, so split and shuffle Niek data and add the rest
+    x, y = annotations[:682], annotations[682:]
+    random.shuffle(x)
+    return x + y
+
+def log_performance(i, model, n, j, N, I):
     global LAST_EVAL
     eval, = evaluate(project, model, annotations=eval_annotations)
     out.writerow([n, i, eval.label, eval.tp, eval.fp, eval.fn, eval.tn])
     LAST_EVAL = eval
-    logging.info("[{n}/{N}][{i}/{I}] {}".format(LAST_EVAL.eval_str(), N=N, I=I, **locals()))
+    logging.info("[{j}/5][{n}/{N}][{i}/{I}] {}".format(LAST_EVAL.eval_str(), **locals()))
 
 
 percs = [.25, .50, .75, 1]
-#percs = [.125, .25, .375, .5, .625, .75, .875, 1.0]
-for perc in percs:
-    n = ceil(len(all_annotations) * perc)
-    annotations = all_annotations[:n]
+percs = [.125, .25, .375, .5, .625, .75, .875, 1.0]
 
-    model = train(project, annotations, I, callback=partial(log_performance, n=n))
-    logging.info("{}: {}".format(len(annotations), LAST_EVAL.eval_str()))
+for j in range(10):
+    # python sorting is guaranteed to be stable, so shuffle then sort on sessionid
+    all_annotations = shuffle_annotations(all_annotations)
+    for perc in percs:
+        n = ceil(len(all_annotations) * perc)
+        annotations = all_annotations[:n]
+
+        model = train(project, annotations, I, callback=partial(log_performance, n=n, j=j, N=N, I=I))
+        logging.info("{}: {}".format(len(annotations), LAST_EVAL.eval_str()))
 
